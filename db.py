954486f45c5769650ca440222b68c0f9c50ce4ca
maxig <maxigaspero@gmail.com>
2023-12-20 03:10:54 -0300

    agregado de tabla Bomba, coordenadas de donde sacar agua

--- db.py
+++ db.py
@@ -79,3 +79,72 @@ def borrarPoint(id):
     cursor.execute(consulta_delete_point)
     conexion.commit()
     conexion.close()
+
+#tabla para tener registro de las bombas de aguas que pueden ser utilizadas en un incendio
+def createTableBomba():
+    conexion = connectBD()
+    cursor = conexion.cursor()
+
+    # Crear una tabla si no existe
+    consulta_creacion_tabla_bomba = """
+        CREATE TABLE IF NOT EXISTS bomba (
+            id SERIAL PRIMARY KEY,
+            startPoints VARCHAR(40),
+            stopPoints VARCHAR(40),
+            description VARCHAR(255)
+        )
+    """
+    cursor.execute(consulta_creacion_tabla)
+
+    # Guardar los cambios y cerrar la conexión
+    conexion.commit()
+    conexion.close()
+
+
+def insertarPoints(startPoint,stopPoint, descripicon=""):
+    conexion = connectBD()
+    cursor = conexion.cursor()
+
+    consulta_insercion_bomba = sql.SQL("INSERT INTO bomba (startPoints, stopPoints, description) VALUES (%s, %s, %s)")
+    datos_point = (startPoint, stopPoint, descripicon)
+    cursor.execute(consulta_insercion_bomba, datos_point)
+
+
+    # Guardar los cambios y cerrar la conexión
+    conexion.commit()
+    conexion.close()
+
+
+def seleccionarBomba():
+    conexion = connectBD()
+    cursor = conexion.cursor()
+
+    consulta_seleccion_point = "SELECT * FROM bomba"
+
+    cursor.execute(consulta_seleccion_bomba)
+
+    resultados = cursor.fetchall()
+    conexion.close()
+
+    return resultados
+
+def seleccionarBomba(id):
+    conexion = connectBD()
+    cursor = conexion.cursor()
+
+    consulta_seleccion_bomba = "SELECT * FROM bomba WHERE id = {}".format(id)
+
+    cursor.execute(consulta_seleccion_bomba)
+
+    resultado = cursor.fetchall()
+    conexion.close()
+    
+    return resultado[0]
+
+def borrarPoint(id):
+    conexion = connectBD()
+    cursor = conexion.cursor()
+    consulta_delete_bomba = sql.SQL("DELETE FROM bomba WHERE id = {}".format(id))
+    cursor.execute(consulta_delete_bomba)
+    conexion.commit()
+    conexion.close()
