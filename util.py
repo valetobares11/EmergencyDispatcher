2380bc262c616605be021d7f8dc4bf66b085b71c
vtobares <valetobares11@gmail.com>
2024-01-24 16:29:54 +0100

    se agrega tareas

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 
+from os import close
 import requests
 from .db import insert,select
 from uu import decode
@@ -6,6 +7,12 @@ from urllib.request import urlopen
 import json
 import re
 from .config import *
+from .db import *
+import pandas as pd
+from pyexcel_ods import get_data
+import psycopg2
+from psycopg2 import sql
+
 
 def geocode_address(address):
     base_url = "https://nominatim.openstreetmap.org/search"
@@ -64,20 +71,72 @@ def insertar_punto(start_point, stop_point, description):
     valores = "{}, {}, '{}'".format(start_point, stop_point, description)
     insert('points', 'startPoint, stopPoint, description', valores)
 
+def get_api_key_google():
+    try:
+        with open(API_KEY_GOOGLE, 'r') as f:
+            clave_api = f.read().strip()
+            return clave_api
+    except FileNotFoundError:
+        print(f"El archivo '{API_KEY_GOOGLE}' no fue encontrado.")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
 
 def obtener_coordenada(address):
     base_url = "https://maps.googleapis.com/maps/api/geocode/json?"
+    api_key = get_api_key_google()
+    
     params = {
         "address": address,
-        "key" : "cambiar_api_key",
+        "key": 'poner_clave',
     }
     
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
         data = response.json()
+        print(data)
         if data:
             # Extrae las coordenadas (latitud y longitud) del primer resultado
             lat = float(data["results"][0]["geometry"]["location"]["lat"])
             lon = float(data["results"][0]["geometry"]["location"]["lng"])
             return lon, lat
     return None
+
+def cargar_pedidos():
+    try:
+        # Lee el archivo ODS
+        data = get_data(PEDIDOS)
+        
+        # Extrae los datos de la primera hoja del archivo ODS
+        sheet_name = list(data.keys())[0]
+        datos = data[sheet_name]
+        
+        # Convierte la lista de listas en un DataFrame
+        df = pd.DataFrame(datos[1:], columns=datos[0])
+        df_filtrado = df.dropna()
+        conexion = connectBD()
+        cursor = conexion.cursor()
+        direccion =''
+        startpoint= ''
+        stoppoint = ''
+        tiempo = ''
+        consulta_insercion = ''
+        
+        for i, fila in df_filtrado.iterrows():
+            for columna, valor in fila.items():
+                if (columna == 'startpoint'): startpoint = valor
+                if (columna == 'stoppoint'): stoppoint = valor
+                if (columna == 'direccion'): direccion = valor
+                if (columna == 'tiempo'): tiempo = valor
+            x,y = obtener_coordenada(direccion+' ' +CIUDAD + ' '+PROVINCIA)
+            stoppoint = '{},{}'.format(x,y)
+            consulta_insercion += ("INSERT INTO pedido (direccion, startpoint, stoppoint, tiempo) VALUES ('{}', '{}', '{}', CURRENT_DATE + INTERVAL '{}' HOUR TO MINUTE); \n".format(direccion, startpoint, stoppoint,tiempo))
+
+        cursor.execute(consulta_insercion)
+        # Guardar los cambios y cerrar la conexión
+        conexion.commit()
+        conexion.close()
+
+    except FileNotFoundError:
+        print(f"El archivo {PEDIDOS} no fue encontrado.")
+    except Exception as e:
+        print(f"Error al procesar el archivo {PEDIDOS}: {str(e)}")
